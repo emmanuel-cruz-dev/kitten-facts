5fdad7abb72b0f4ce3eccada1c6d3d29f44aea0d
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-13 14:26:54 -0300

    prueba de test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,9 +1,18 @@
 // @ts-check
 import { test, expect } from "@playwright/test";
 
-test("has title", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+const CAT_PREFIX_IMAGE_URL = "https://cataas.com";
+const LOCALHOST_URL = "https://localhost:5173";
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test("app shows random fact and image", async ({ page }) => {
+  await page.goto(LOCALHOST_URL);
+
+  const text = await page.getByRole("paragraph");
+  const image = await page.getByRole("img");
+
+  const textContent = await text.textContent();
+  const imageSrc = await image.getAttribute("src");
+
+  await expect(textContent?.length).toBeGreaterThan(0);
+  await expect(imageSrc?.startsWith(CAT_PREFIX_IMAGE_URL)).toBeTruthy();
 });
